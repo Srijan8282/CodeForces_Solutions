3f58f800686f9bc6260b82e340daff38b51200f0
Srijan Kundu Chowdhury <115431775+Srijan8282@users.noreply.github.com>
2024-01-04 23:47:55 +0530

    Add files via upload

---
+++ A_Two_Vessels.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin>>t;
+
+    while(t--){
+        double a,b,c;
+        cin>>a>>b>>c;
+
+        double mid=(a+b)/2;
+
+        double extra=abs(a-mid);
+
+        cout<<ceil((float)extra/(float)c)<<endl;
+    }
+
+
+    return 0;
+}
+
